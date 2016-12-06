36975283a4a8cc2098e6a56e652ea07d3213a065
Henrik Ravn <hravnx@gmail.com>
2016-12-06 10:36:27 +0100

    Initial commit, with setup and test working in VS2015 and dotnet CLI

---
+++ test/CliHelpers.Tests/CliOptionTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace CliHelpers.Tests
+{
+    
+    public class CliOptionTests
+    {
+        [Fact]
+        public void OptionIsFine()
+        {
+            Assert.Equal(2, 1+1);
+        }
+    }
+}
